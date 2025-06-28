b9481cbc2d88b3243aea8e341eea0b28c69e44c3
andy <has32photo@gmail.com>
2025-06-28 20:25:38 +0300

    Update file

---
+++ DE-101/Module4/data/airflow/first_DAG.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+# Функция, которая будет выполняться в задаче
+def my_task():
+    print("Hello, Airflow!")
+
+# Определение DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
+
+dag = DAG(
+    'simple_dag',
+    default_args=default_args,
+    description='A simple DAG example',
+    schedule_interval='@daily',
+)
+
+# Задача 1: Начало
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+# Задача 2: Выполнение Python функции
+run_my_task = PythonOperator(
+    task_id='run_my_task',
+    python_callable=my_task,
+    dag=dag,
+)
+
+# Задача 3: Завершение
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+# Определение порядка выполнения задач
+start >> run_my_task >> end
